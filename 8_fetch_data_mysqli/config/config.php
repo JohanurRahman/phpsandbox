030ae4d33ec6e6c3197cbc0e36a4813edb98f77f
JohanurRahman <johanur.rahman@gmail.com>
2019-01-23 16:41:42 +0600

    Added fetching from mysqli

---
+++ 8_fetch_data_mysqli/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+    define('ROOT_URL', 'http://localhost/phpsandbox/8_fetch_data_mysqli/');
+
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', 'root');
+    define('DB_NAME', 'phpblog');
+?>
---
+++ 8_fetch_data_mysqli/config/db.php
@@ -0,0 +1,14 @@
+<?php 
+
+    // Create Connection
+    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+    if(mysqli_connect_errno()) {
+
+        // Connection failed
+        echo 'Failed to connect to MySQL '. mysqli_connect_errno();
+
+    }
+
+
+?>
